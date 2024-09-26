7fedca4e8c874776522f1ae398ea9fcdfe16fef2
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-26 14:22:38 -0500

    first commit

---
+++ sets_tuples_lists.py
@@ -0,0 +1,80 @@
+#collection =single "variable" used to store multiple values 
+#list = [] ordered and changable. Duplicates OK
+#set = {} unordered and immutab;le, but add/remove OK. NO dublicates
+#tuple= () orderted and unchangable., Duplicates OK. FASTER 
+
+fruits = ['apple' ,  'orange' , 'banana', 'coconut', 'pineapple', 'kiwi', 'strawberry' , 'papya' , 'mango' ,]
+print (fruits[1:3])
+
+# list starts with 0, 0=apple, 4=put of range 
+
+print (len(fruits))
+# #this finds length 
+
+print (fruits[::2])
+# #this gives you every second word 
+
+print(fruits[::-1])
+# #reverses order of list 
+
+# for fruit in fruits:
+#first thing with itteration
+#DONT NAME VARIABLE ONE LETTER
+#fruit represent object in list 
+#itteration is looping throught the whole thing 
+#will loop through each time 
+#iteration 1- fruit = apple
+#itteration 2- fruit = orange 
+
+print (dir (fruits))
+#dir-gives all atributes in a list 
+
+# print(help(fruits))
+#gives you documentation for what it does 
+
+len(fruits)
+# #gives the length of the list
+
+print ("apple" in fruits )
+ #tells you if element is in a list
+ #boolean 
+print ("tomato " in fruits)
+
+#lists are ordered and changable, duplicates are ok 
+
+fruits[0] = "pineapple"
+#changes the value of the "0" in the list 
+
+fruits [2] = "grape"
+
+print(fruits)
+
+for fruit in fruits:
+    print(fruit)
+
+fruits.append("pineapple")
+# . append will add something to the end of the list 
+
+fruits.remove('grape')
+# .rempove will remove
+
+fruits.insert(0, 'pineapple')
+#will put that value in the given spot (pineapple in spot 1)
+
+fruits.sort()
+#alphabetical order
+
+#fruits.reverse()
+#reverse order from how you put them in 
+
+fruits #??
+#prints in reverse alhabetical order 
+
+fruits.clear()
+# ??
+
+print (fruits.index ('apple'))
+#finds the location/index
+
+for fruit in fruits:
+    print(fruit)
